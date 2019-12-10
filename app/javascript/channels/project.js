09ef989e01ae37a9d2e16ee2786265a7b6f9859f
Ken Narita <hokkaider-0413@outlook.jp>
2019-12-10 12:31:57 +0900

    トップページのビュー

--- app/javascript/channels/project.js
+++
@@ -1,6 +0,0 @@
-$('.menu__item').on('click',function(){
-  var index = $('.menu__item li').index(this);
-  console.log(index)
-  $('.menu-list').removeClass('active');
-  $('.menu-list').eq(index).addClass('active');
-});
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "project#index"
-  resources :project
-
+  resources :project, only:[ :index, :about, :work ] do
+    collection do
+      get :index
+      get :about 
+      get :work
+    end
+  end
 end
